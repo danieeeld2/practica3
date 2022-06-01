2ea2ca9dabd2cfc1bef1ead8614be618db76310f
danieeeld2 <danieeeld2@gmail.com>
2022-06-01 11:23:51 +0200

    PodaAlfaBeta

--- src/AIPlayer.cpp
+++ src/AIPlayer.cpp
@@ -1,5 +1,5 @@
-# include "AIPlayer.h"
-# include "Parchis.h"
+#include "AIPlayer.h"
+#include "Parchis.h"
 
 const double masinf = 9999999999.0, menosinf = -9999999999.0;
 const double gana = masinf - 1, pierde = menosinf + 1;
@@ -7,9 +7,10 @@ const int num_pieces = 4;
 const int PROFUNDIDAD_MINIMAX = 4;  // Umbral maximo de profundidad para el metodo MiniMax
 const int PROFUNDIDAD_ALFABETA = 6; // Umbral maximo de profundidad para la poda Alfa_Beta
 
-bool AIPlayer::move(){
+bool AIPlayer::move()
+{
     cout << "Realizo un movimiento automatico" << endl;
-    
+
     color c_piece;
     int id_piece;
     int dice;
@@ -21,31 +22,37 @@ bool AIPlayer::move(){
     return true;
 }
 
-void AIPlayer::think(color & c_piece, int & id_piece, int & dice) const{
-    switch(id){
-        case 0:
-            thinkAleatorio(c_piece,id_piece,dice);
-            break;
-        case 1:
-            thinkAleatorioMasInteligete(c_piece,id_piece,dice);
-            break;
-        case 2:
-            thinkFichasMasAdelante(c_piece,id_piece,dice);
-            break;
-        case 3:
-            thinkMejorOpcion(c_piece,id_piece,dice);
-            break;
-    }
+void AIPlayer::think(color &c_piece, int &id_piece, int &dice) const
+{
+    // switch(id){
+    //     case 0:
+    //         thinkAleatorio(c_piece,id_piece,dice);
+    //         break;
+    //     case 1:
+    //         thinkAleatorioMasInteligete(c_piece,id_piece,dice);
+    //         break;
+    //     case 2:
+    //         thinkFichasMasAdelante(c_piece,id_piece,dice);
+    //         break;
+    //     case 3:
+    //         thinkMejorOpcion(c_piece,id_piece,dice);
+    //         break;
+    // }
+    double alpha = menosinf;
+    double beta = masinf;
+    double valor = Poda_AlfaBeta(*actual, jugador, 0, PROFUNDIDAD_ALFABETA, c_piece, id_piece, dice, alpha, beta, ValoracionTest);
+    cout << "Valor MiniMax: " << valor << "  Accion: " << str(c_piece) << " " << id_piece << " " << dice << endl;
 }
 
-void AIPlayer::thinkAleatorio(color & c_piece, int & id_piece, int & dice) const{
+void AIPlayer::thinkAleatorio(color &c_piece, int &id_piece, int &dice) const
+{
     // IMPLEMENTACIÓN INICIAL DEL AGENTE
     // Esta implementación realiza un movimiento aleatorio.
-    // Se proporciona como ejemplo, pero se debe cambiar por una que realice un movimiento inteligente 
-    //como lo que se muestran al final de la función.
-    
-    // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por referencia) los valores, 
-    //respectivamente, de:
+    // Se proporciona como ejemplo, pero se debe cambiar por una que realice un movimiento inteligente
+    // como lo que se muestran al final de la función.
+
+    // OBJETIVO: Asignar a las variables c_piece, id_piece, dice (pasadas por referencia) los valores,
+    // respectivamente, de:
     // - color de ficha a mover
     // - identificador de la ficha que se va a mover
     // - valor del dado con el que se va a mover la ficha.
@@ -67,10 +74,12 @@ void AIPlayer::thinkAleatorio(color & c_piece, int & id_piece, int & dice) const
     current_pieces = actual->getAvailablePieces(c_piece, dice);
 
     // Si tengo fichas para el dado elegido muevo una al azar.
-    if(current_pieces.size() > 0){
+    if (current_pieces.size() > 0)
+    {
         id_piece = current_pieces[rand() % current_pieces.size()];
     }
-    else{
+    else
+    {
         // Si no tengo fichas para el dado elegido, pasa turno (la macro SKIP_TURN me permite no mover).
         id_piece = SKIP_TURN;
     }
@@ -103,7 +112,8 @@ void AIPlayer::thinkAleatorio(color & c_piece, int & id_piece, int & dice) const
     */
 }
 
-void AIPlayer::thinkAleatorioMasInteligete(color & c_piece, int & id_piece, int & dice) const{
+void AIPlayer::thinkAleatorioMasInteligete(color &c_piece, int &id_piece, int &dice) const
+{
     // El color de ficha que se va a mover
     c_piece = actual->getCurrentColor();
 
@@ -117,21 +127,26 @@ void AIPlayer::thinkAleatorioMasInteligete(color & c_piece, int & id_piece, int 
 
     // En vez de elegir un dado al azar, miro primero cuáles tienen fichas que se puedan mover
     vector<int> current_dices_que_pueden_mover_ficha;
-    for(int i=0; i<current_dices.size(); i++){
+    for (int i = 0; i < current_dices.size(); i++)
+    {
         // Se obtiene el vector de fichas que se pueden mover para el dado elegido
         current_pieces = actual->getAvailablePieces(c_piece, current_dices[i]);
 
         // Si se pueden mover fichas para el dado actual, lo añado al vector de dados que pueden mover fichas
-        if(current_pieces.size() > 0){
+        if (current_pieces.size() > 0)
+        {
             current_dices_que_pueden_mover_ficha.push_back(current_dices[i]);
         }
     }
 
     // Si no tengo ningún dado que pueda mover fichas, paso de turno con un dado al azar
-    if(current_dices_que_pueden_mover_ficha.size() == 0){
+    if (current_dices_que_pueden_mover_ficha.size() == 0)
+    {
         dice = current_dices[rand() % current_dices.size()];
         id_piece = SKIP_TURN;
-    }else{
+    }
+    else
+    {
         // En caso contrario elijo un dado de forma aleatoria entre los que pueden mover ficha
         dice = current_dices_que_pueden_mover_ficha[rand() % current_dices_que_pueden_mover_ficha.size()];
 
@@ -143,7 +158,8 @@ void AIPlayer::thinkAleatorioMasInteligete(color & c_piece, int & id_piece, int 
     }
 }
 
-void AIPlayer::thinkFichasMasAdelante(color & c_piece, int & id_piece, int & dice) const{
+void AIPlayer::thinkFichasMasAdelante(color &c_piece, int &id_piece, int &dice) const
+{
     // Elijo el dado haciendo lo mismo que el jugador anterior
     thinkAleatorioMasInteligete(c_piece, id_piece, dice);
     // Tras llamar a esta función, ya tengo en dice el número de dado que quiero usar
@@ -153,33 +169,39 @@ void AIPlayer::thinkFichasMasAdelante(color & c_piece, int & id_piece, int & dic
     vector<int> current_pieces = actual->getAvailablePieces(c_piece, dice);
 
     int id_ficha_mas_adelantada = -1;
-    int min_distancia_meta= 9999;
-    for(int i=0; i<current_pieces.size(); i++){
+    int min_distancia_meta = 9999;
+    for (int i = 0; i < current_pieces.size(); i++)
+    {
         // distanceToGoal(color, id) devuelve ña distancia a la meta de la ficha [id] del color que le indique
         int distancia_meta = actual->distanceToGoal(c_piece, current_pieces[i]);
-        if(distancia_meta < min_distancia_meta){
+        if (distancia_meta < min_distancia_meta)
+        {
             min_distancia_meta = distancia_meta;
             id_ficha_mas_adelantada = current_pieces[i];
         }
     }
 
     // Si no he encontrado ninguna ficha, paso turno
-    if(id_ficha_mas_adelantada == -1){
+    if (id_ficha_mas_adelantada == -1)
+    {
         id_piece = SKIP_TURN;
-    }else{ // En caso contrario, moveré la más adelantada
+    }
+    else
+    { // En caso contrario, moveré la más adelantada
         id_piece = id_ficha_mas_adelantada;
     }
 }
 
-void AIPlayer::thinkMejorOpcion(color & c_piece, int & id_piece, int & dice) const{
+void AIPlayer::thinkMejorOpcion(color &c_piece, int &id_piece, int &dice) const
+{
     // Vamos a mirar todos los posibles movimientos del jugador actual accediendo a los hijos del estado actual
 
     // generateNextMove va iterando sobre cada hijo. Le paso la acción del último movimiento sobre
     // el que he iterado y me devolverá el siguiente. Inicialmente, cuando aún no he hecho ningún
     // movimiento, lo inicializo así:
-    color last_c_piece = none;  // El color de la última ficha que se movió
-    int last_id_piece = -1;     // El ide de la última ficha que se movió
-    int last_dice = -1;         // El dado que se usó en el último movimiento
+    color last_c_piece = none; // El color de la última ficha que se movió
+    int last_id_piece = -1;    // El ide de la última ficha que se movió
+    int last_dice = -1;        // El dado que se usó en el último movimiento
 
     // Cuando ya he recorrido todos los hijos, la función devuelve el estado actual. De esta forma puedo saber
     // cuando paro de iterar
@@ -188,25 +210,32 @@ void AIPlayer::thinkMejorOpcion(color & c_piece, int & id_piece, int & dice) con
 
     bool me_quedo_con_esta_accion = false;
 
-    while(!(siguiente_hijo == *actual) && !me_quedo_con_esta_accion){
-        if(siguiente_hijo.isEatingMove() or // Si con este movimiento como ficha
-           siguiente_hijo.isGoalMove() or // Si con este movimiento llego a la meta
-           (siguiente_hijo.gameOver() and siguiente_hijo.getWinner() == this->jugador) // Si con este movimiento gano la partida
-        ){
+    while (!(siguiente_hijo == *actual) && !me_quedo_con_esta_accion)
+    {
+        if (siguiente_hijo.isEatingMove() or                                            // Si con este movimiento como ficha
+            siguiente_hijo.isGoalMove() or                                              // Si con este movimiento llego a la meta
+            (siguiente_hijo.gameOver() and siguiente_hijo.getWinner() == this->jugador) // Si con este movimiento gano la partida
+        )
+        {
             // Me quedo con la acción actual
             me_quedo_con_esta_accion = true;
-        }else{
+        }
+        else
+        {
             // Genero el siguiente hijo
             siguiente_hijo = actual->generateNextMove(last_c_piece, last_id_piece, last_dice);
         }
     }
 
     // Si he encontrado una acción que me interesa, la guardo en las variables por referencia
-    if(me_quedo_con_esta_accion){
+    if (me_quedo_con_esta_accion)
+    {
         c_piece = last_c_piece;
         id_piece = last_id_piece;
         dice = last_dice;
-    }else{
+    }
+    else
+    {
         // Si no, muevo la ficha más adelantada como antes
         thinkFichasMasAdelante(c_piece, id_piece, dice);
     }
@@ -215,7 +244,6 @@ void AIPlayer::thinkMejorOpcion(color & c_piece, int & id_piece, int & dice) con
 double AIPlayer::ValoracionTest(const Parchis &estado, int jugador)
 {
     // Heurística de prueba proporcionada para validar el funcionamiento del algoritmo de búsqueda.
-
 
     int ganador = estado.getWinner();
     int oponente = (jugador + 1) % 2;
@@ -282,3 +310,55 @@ double AIPlayer::ValoracionTest(const Parchis &estado, int jugador)
     }
 }
 
+double AIPlayer::Poda_AlfaBeta(const Parchis &actual, int jugador, int profundidad, int profundidad_max, color &c_piece, int &id_piece, int &dice, double alpha, double beta, double (*heuristic)(const Parchis &, int)) const
+{
+    if (profundidad == profundidad_max || actual.gameOver())
+    {
+        return heuristic(actual, jugador);
+    }
+
+    color last_c = none;
+    int last_piece = -1;
+    int last_dice = -1;
+    Parchis hijo = actual.generateNextMoveDescending(last_c, last_piece, last_dice);
+
+    if (actual.getCurrentPlayerId() == jugador)
+    {
+        while (!(hijo == actual))
+        {
+            auto valor_poda = Poda_AlfaBeta(hijo, jugador, profundidad + 1, profundidad_max, last_c, last_piece, last_dice, alpha, beta, heuristic);
+            if (alpha < valor_poda)
+            {
+                alpha = valor_poda;
+                if (profundidad == 0)
+                {
+                    c_piece = last_c;
+                    id_piece = last_piece;
+                    dice = last_dice;
+                }
+            }
+
+            if (beta <= alpha)
+                break;
+
+            hijo = actual.generateNextMoveDescending(last_c, last_piece, last_dice);
+        }
+
+        return alpha;
+    }
+    else
+    {
+        while (!(hijo == actual))
+        {
+            auto valor_poda = Poda_AlfaBeta(hijo, jugador, profundidad + 1, profundidad_max, last_c, last_piece, last_dice, alpha, beta, heuristic);
+            if (valor_poda > beta)
+                beta = valor_poda;
+
+            if (beta <= alpha)
+                break;
+
+            hijo = actual.generateNextMoveDescending(last_c, last_piece, last_dice);
+        }
+        return beta;
+    }
+}
